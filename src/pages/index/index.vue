cf64b7bc3276592192a2007df12e4607f02d5a38
Zhaofan-Su <1015655239@qq.com>
2020-10-26 17:35:31 +0800

    init jianjian repo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,180 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view>
+    <!-- 背景地图 -->
+    <image
+      id="bg-map"
+      src="/static/map.png"
+      :style="{ width: screenWidth + 'px', height: screenHeight + 'px' }"
+    ></image>
+    <!-- 顶部菜单按钮 -->
+    <view class="padding" style="position: absolute">
+      <button class="cu-btn cuIcon bg-cyan">
+        <text class="xslg cuIcon-list"></text>
+      </button>
+    </view>
+
+    <!-- 底部三个按钮 -->
+    <view
+      id="bottom-bar"
+      class="padding flex flex-wrap justify-between align-center"
+      :style="{ top: screenHeight * 0.9 + 'px' }"
+    >
+      <switch
+        @change="showAcQrModal"
+        :class="acceptQrcode ? 'checked' : ''"
+        color="#39B54A"
+      ></switch>
+      <button class="cu-btn cuIcon bg-cyan lg">
+        <text class="text-xsl cuIcon-add"></text>
+      </button>
+      <button class="cu-btn cuIcon bg-cyan" @tap="sendQrModal = true">
+        <text class="cuIcon-qr_code"></text>
+      </button>
+    </view>
+
+    <!-- 开启位置权限模态框 -->
+    <view class="cu-modal" :class="acQrModal == true ? 'show' : ''">
+      <view class="cu-dialog">
+        <view class="cu-bar bg-white justify-end">
+          <view class="content">共享位置吗？</view>
+          <view class="action" @tap="acQrModal = false">
+            <text class="cuIcon-close text-red"></text>
+          </view>
+        </view>
+        <view class="padding-xl">
+          开启位置共享之后可以收到周围的人发来的二维码哦！
+        </view>
+        <view class="cu-bar bg-white justify-end">
+          <view class="action">
+            <button class="cu-btn line-green text-green" @tap="acQrOff">
+              取消
+            </button>
+            <button class="cu-btn bg-green margin-left" @tap="acQrOn">
+              确定
+            </button>
+          </view>
+        </view>
+      </view>
+    </view>
+
+    <!-- 送码模态框 -->
+    <view class="cu-modal" :class="sendQrModal == true ? 'show' : ''">
+      <view class="cu-dialog">
+        <view class="cu-bar bg-white justify-end">
+          <view class="content">给周围的人送码？</view>
+          <view class="action" @tap="sendQrModal = false">
+            <text class="cuIcon-close text-red"></text>
+          </view>
+        </view>
+        <view class="padding-xl"> 这是你的二维码 </view>
+        <view class="cu-bar bg-white justify-end">
+          <view class="action">
+            <button
+              class="cu-btn line-green text-green"
+              @tap="sendQrModal = false"
+            >
+              取消
+            </button>
+            <button class="cu-btn bg-green margin-left" @tap="sendQrCode">
+              确定
+            </button>
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+export default {
+  data() {
+    return {
+      screenHeight: 0,
+      screenWidth: 0,
+      acceptQrcode: false,
+      acQrModal: false,
+      sendQrModal: false,
+    };
+  },
+  onLoad() {
+    this.setMapSize();
+  },
+  methods: {
+    setMapSize() {
+      var that = this;
+      uni.getSystemInfo({
+        success: function (res) {
+          that.screenHeight = res.windowHeight;
+          that.screenWidth = res.windowWidth;
+        },
+      });
+    },
+    showAcQrModal() {
+      if (!this.acceptQrcode) {
+        // 显示同意收码对话框
+        this.acQrModal = true;
+      } else {
+        // TODO:这里可能要再做一个对话框
+        this.acceptQrcode = false;
+      }
+    },
+    acQrOn() {
+      this.acceptQrcode = true;
+      this.acQrModal = false;
+    },
+    acQrOff() {
+      // TODO:状态转换有问题，5555
+      this.acceptQrcode = false;
+      this.acQrModal = false;
+    },
+    sendQrCode() {
+      // TODO:发送请求
 
-		},
-		methods: {
+      //   uni.request({
+      //     url: "",
+      //     data: {
+      //       text: "uni.request",
+      //     },
+      //     success: (res) => {
+      //       uni.showToast({
+      //         title: "送码成功！",
+      //         mask: true,
+      //         duration: 1500,
+      //       });
+      //     },
+      //     fail: (res) => {
+      //       uni.showToast({
+      //         title: "网络有问题呀，再试试吧",
+      //         mask: true,
+      //         icon: "none",
+      //         duration: 1500,
+      //       });
+      //     },
+      //   });
 
-		}
-	}
+      // 送码成功提醒
+      // 先把外层的modal弄掉
+      this.sendQrModal = false;
+      uni.showToast({
+        title: "送码成功！",
+        mask: true,
+        duration: 1500,
+        fail: (res) => {
+          console.log("接口调用失败：" + res);
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+#bg-map {
+  z-index: -1;
+  position: absolute;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+#bottom-bar {
+  position: relative;
+}
 </style>
