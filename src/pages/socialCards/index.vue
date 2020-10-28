b70998559f395b528e09d61036e21554c1428d55
Zhaofan-Su <1015655239@qq.com>
2020-10-28 23:33:01 +0800

    some little change

--- src/pages/socialCards/index.vue
+++ src/pages/socialCards/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-    <cu-custom bgColor="bg-gradual-cyan" :isBack="true"
+    <cu-custom bgColor="bg-gradual-blue" :isBack="true"
       ><block slot="backText">返回</block
       ><block slot="content">我的名片墙</block></cu-custom
     >
